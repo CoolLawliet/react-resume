b33058edf75fbaf1d1631f6d4981dac6fd3b0475
nodejslhk <531549304@qq.com>
2020-04-10 01:40:56 +0800

    Footer

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer(props) {
+    return (
+        <div>
+            <footer className="bg-dark text-white mt-5 p-4 text-center">
+                Copyright &copy; 2020 自由项目
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
