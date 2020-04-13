9530ec3fba93ce6d84d5418bb9ff2e255ced1a8f
nodejslhk <531549304@qq.com>
2020-04-14 00:48:13 +0800

    解析token

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,31 +1,43 @@
 import axios from 'axios'
+import jwt_decode from 'jwt-decode'
 import setAuthToken from "../utils/setAuthToken";
-import {GET_ERRORS} from './types'
+import {GET_ERRORS,SET_CURRENT_USER} from './types'
 
-export const registerUser=(userData,history)=>dispatch=>{
+export const registerUser = (userData, history) => dispatch => {
     // 请求
-    axios.post('/api/users/register',userData)
-        .then(res=> history.push('/login'))
-        .catch(err=>
-        dispatch({
-            type:GET_ERRORS,
-            payload:err.response.data
-        }))
+    axios.post('/api/users/register', userData)
+        .then(res => history.push('/login'))
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            }))
 }
 
 //登录
-export const loginUser = userData=>dispatch=>{
-    axios.post("/api/users/login",userData)
-        .then(res=> {
+export const loginUser = userData => dispatch => {
+    axios.post("/api/users/login", userData)
+        .then(res => {
             const {token} = res.data
             //存储token
-            localStorage.setItem('jwtToken',token)
+            localStorage.setItem('jwtToken', token)
             //设置axios的headers token
             setAuthToken(token)
+            //解析token
+            const decoded = jwt_decode(token)
+            dispatch(setCurrentUser(decoded))
         })
-        .catch(err=>
+        .catch(err =>
             dispatch({
-                type:GET_ERRORS,
-                payload:err.response.data
-            }))
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        )
+}
+
+export const setCurrentUser = decoded=>{
+    return {
+        type:SET_CURRENT_USER,
+        payload:decoded
+    }
 }
