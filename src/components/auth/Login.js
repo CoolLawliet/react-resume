5c6cb95e9f98828cbb34fb8556e5180517ce4361
nodejslhk <531549304@qq.com>
2020-04-14 02:07:19 +0800

    输入框组件使用

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux'
 import {PropTypes} from 'prop-types'
 import {loginUser} from "../../actions/authActions";
-import classnames from 'classnames'
+import TextFieldGroup from "../../common/TextFieldGroup";
 
 class Login extends Component {
     constructor() {
@@ -58,42 +58,22 @@ class Login extends Component {
                             <h1 className="display-4 text-center">登录</h1>
                             <p className="lead text-center">用正确的账号登录</p>
                             <form onSubmit={this.onSubmit}>
-                                <div className="form-group">
-                                    <input
-                                        type="email"
-                                        className={classnames('form-control form-control-lg',{
-                                            'is-invalid':errors.email
-                                        })}
-                                        placeholder="邮箱地址"
-                                        name="email"
-                                        value={this.state.email}
-                                        onChange={this.onChange}
+                                <TextFieldGroup
+                                    type="email"
+                                    placeholder="邮箱地址"
+                                    name="email"
+                                    value={this.state.email}
+                                    onChange={this.onChange}
+                                    error={errors.email}
                                     />
-                                    {
-                                        errors.email && (
-                                            <div className="invalid-feedback">
-                                                {errors.email}
-                                            </div>)
-                                    }
-                                </div>
-                                <div className="form-group">
-                                    <input
-                                        type="password"
-                                        className={classnames('form-control form-control-lg',{
-                                            'is-invalid':errors.password
-                                        })}
-                                        placeholder="密码"
-                                        name="password"
-                                        value={this.state.password}
-                                        onChange={this.onChange}
-                                    />
-                                    {
-                                        errors.password && (
-                                            <div className="invalid-feedback">
-                                                {errors.password}
-                                            </div>)
-                                    }
-                                </div>
+                                <TextFieldGroup
+                                    type="password"
+                                    placeholder="密码"
+                                    name="password"
+                                    value={this.state.password}
+                                    onChange={this.onChange}
+                                    error={errors.password}
+                                />
                                 <input type="submit" className="btn btn-info btn-block mt-4"/>
                             </form>
                         </div>
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types'
-import classnames from 'classnames'
 import {withRouter} from 'react-router-dom'
+import TextFieldGroup from "../../common/TextFieldGroup";
 /**react-redux 的两个主要功能
 * connect:用于从UI组件生成容器组件，将两种组件连起来
 * Provider:可以让组件及子组件拿到state
@@ -66,80 +66,38 @@ class Register extends Component {
                                 <h1 className="display-4 text-center">注册</h1>
                                 <p className="lead text-center">创建新的账户</p>
                                 <form onSubmit={this.onSubmit}>
-                                    <div className="form-group">
-                                        <input
-                                            type="text"
-                                            className={classnames('form-control form-control-lg',{
-                                                'is-invalid':errors.name
-                                            })}
-                                            // className="form-control form-control-lg"
-                                            placeholder="用户名"
-                                            name="name"
-                                            value={this.state.name}
-                                            onChange={this.onChange}
-                                            />
-                                        {
-                                            errors.name && (
-                                                <div className="invalid-feedback">
-                                                    {errors.name}
-                                                </div>)
-                                        }
-                                    </div>
-                                    <div className="form-group">
-                                        <input type="email"
-                                               // className="form-control form-control-lg"
-                                               className={classnames('form-control form-control-lg',{
-                                                   'is-invalid':errors.email
-                                               })}
-                                               placeholder="邮箱地址"
-                                               name="email"
-                                               value={this.state.email}
-                                               onChange={this.onChange}
-
-                                        />
-                                        {
-                                            errors.email && (
-                                                <div className="invalid-feedback">
-                                                    {errors.email}
-                                                </div>)
-                                        }
-                                        <small
-                                            className="form-text text-muted">我们使用了gravatar全球公认头像,如果需要有头像显示,请使用在gravatar注册的邮箱</small>
-                                    </div>
-                                    <div className="form-group">
-                                        <input type="password"
-                                               // className="form-control form-control-lg"
-                                               className={classnames('form-control form-control-lg',{
-                                                   'is-invalid':errors.password
-                                               })}
-                                               placeholder="密码" name="password"
-                                               value={this.state.password}
-                                               onChange={this.onChange}
-                                        />
-                                        {
-                                            errors.password && (
-                                                <div className="invalid-feedback">
-                                                    {errors.password}
-                                                </div>)
-                                        }
-                                    </div>
-                                    <div className="form-group">
-                                        <input type="password"
-                                               // className="form-control form-control-lg"
-                                               className={classnames('form-control form-control-lg',{
-                                                   'is-invalid':errors.password2
-                                               })}
-                                               placeholder="确认密码" name="password2"
-                                               value={this.state.password2}
-                                               onChange={this.onChange}
-                                        />
-                                        {
-                                            errors.password2 && (
-                                                <div className="invalid-feedback">
-                                                    {errors.password2}
-                                                </div>)
-                                        }
-                                    </div>
+                                    <TextFieldGroup
+                                        placeholder="用户名"
+                                        name="name"
+                                        value={this.state.name}
+                                        onChange={this.onChange}
+                                        error={errors.name}
+                                    />
+                                    <TextFieldGroup
+                                        type="email"
+                                        placeholder="邮箱地址"
+                                        name="email"
+                                        value={this.state.email}
+                                        onChange={this.onChange}
+                                        error={errors.email}
+                                        info="我们使用了gravatar全球公认头像,如果需要有头像显示,请使用在gravatar注册的邮箱"
+                                    />
+                                    <TextFieldGroup
+                                        type="password"
+                                        placeholder="密码"
+                                        name="password"
+                                        value={this.state.password}
+                                        onChange={this.onChange}
+                                        error={errors.password}
+                                    />
+                                    <TextFieldGroup
+                                        type="password2"
+                                        placeholder="确认密码"
+                                        name="password2"
+                                        value={this.state.password2}
+                                        onChange={this.onChange}
+                                        error={errors.password2}
+                                    />
                                     <input type="submit" className="btn btn-info btn-block mt-4"/>
                                 </form>
                             </div>
