182a2133097b05eb7768d8e0bec765d94b56b91c
nodejslhk <531549304@qq.com>
2020-04-15 23:51:08 +0800

    添加个人经历结构

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Register from "./components/auth/Register";
 
 import Dashboard from "./components/dashboard/Dashboard";
 import EditProfile from "./components/edit-profile/EditProfile";
+import AddExperience from "./components/add-credentials/AddExperience";
 
 import PrivateRoute from "./common/PrivateRoute";
 
@@ -67,6 +68,9 @@ class App extends Component {
                             </Switch>
                             <Switch>
                                 <PrivateRoute exact path='/edit-profile' component={EditProfile}/>
+                            </Switch>
+                            <Switch>
+                                <PrivateRoute exact path='/add-experience' component={AddExperience}/>
                             </Switch>
                         </div>
                         <Footer/>
