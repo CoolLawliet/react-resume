16730035d202096ff5015985752f7a02a85b9c45
nodejslhk <531549304@qq.com>
2020-04-15 23:21:22 +0800

    编辑个人信息

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 
 import Dashboard from "./components/dashboard/Dashboard";
+import EditProfile from "./components/edit-profile/EditProfile";
 
 import PrivateRoute from "./common/PrivateRoute";
 
@@ -63,6 +64,9 @@ class App extends Component {
                             </Switch>
                             <Switch>
                                 <PrivateRoute exact path='/create-profile' component={CreateProfile}/>
+                            </Switch>
+                            <Switch>
+                                <PrivateRoute exact path='/edit-profile' component={EditProfile}/>
                             </Switch>
                         </div>
                         <Footer/>
