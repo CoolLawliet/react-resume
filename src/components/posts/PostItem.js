a44cb3fcef64c73b3931a34df70f89155bfa7275
nodejslhk <531549304@qq.com>
2020-04-18 18:35:18 +0800

    删除评论 点赞 取消点赞

--- src/components/posts/PostItem.js
+++ src/components/posts/PostItem.js
@@ -1,20 +1,42 @@
 import React, {Component} from 'react'
 import classnames from 'classnames';
 import {connect} from 'react-redux';
-import PropTypes from 'prop-types';
+import {PropTypes} from 'prop-types';
 import {Link} from 'react-router-dom';
 import {deletePost, addLike, removeLike} from '../../actions/postActions';
 
 class PostItem extends Component {
+
+    onDeleteClick(id) {
+        this.props.deletePost(id);
+    }
+
+    onLikeClick(id) {
+        this.props.addLike(id);
+    }
+
+    onUnlikeClick(id) {
+        this.props.removeLike(id);
+    }
+
+    findUserLike(likes) {
+        const {auth} = this.props;
+
+        if (likes.filter(like => like.user === auth.user.id).length > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     render() {
-        const {post, auth, showActions} = this.props
+        const {post, auth, showActions} = this.props;
         return (
             <div className="card card-body mb-3">
                 <div className="row">
                     <div className="col-md-2">
                         <a href="profile.html">
-                            <img className="rounded-circle d-nonse d-md-block"
-                                 src={post.avatar}
+                            <img className="rounded-circle d-nonse d-md-block" src={post.avatar}
                                  alt=""/>
                         </a>
                         <br/>
@@ -25,19 +47,20 @@ class PostItem extends Component {
                         {
                             showActions ? (
                                 <span>
-                  <button onClick={this.onLikeClick.bind(this, post._id)} type="button" className="btn btn-light mr-1">
-                    <i className={classnames("fas fa-thumbs-up", {
-                        'text-info': this.findUserLike(post.likes)
-                    })}/>
-                    <span className="badge badge-light">{post.likes.length}</span>
-                  </button>
-                  <button onClick={this.onUnlikeClick.bind(this, post._id)} type="button"
-                          className="btn btn-light mr-1">
-                    <i className="text-secondary fas fa-thumbs-down"/>
-                  </button>
-                  <Link to={`/post/${post._id}`} className="btn btn-info mr-1">
-                    鼓励留言
-                  </Link>
+                                    <button onClick={this.onLikeClick.bind(this, post._id)} type="button"
+                                            className="btn btn-light mr-1">
+                                        <i className={classnames("fas fa-thumbs-up", {
+                                            'text-info': this.findUserLike(post.likes)
+                                        })}/>
+                                        <span className="badge badge-light">{post.likes.length}</span>
+                                    </button>
+                                    <button onClick={this.onUnlikeClick.bind(this, post._id)} type="button"
+                                            className="btn btn-light mr-1">
+                                        <i className="text-secondary fas fa-thumbs-down"/>
+                                    </button>
+                                    <Link to={`/post/${post._id}`} className="btn btn-info mr-1">
+                                        鼓励留言
+                                    </Link>
                                     {
                                         post.user === auth.user.id ? (
                                             <button
@@ -48,15 +71,18 @@ class PostItem extends Component {
                                             </button>
                                         ) : null
                                     }
-                </span>
+                                </span>
                             ) : null
                         }
-
                     </div>
                 </div>
             </div>
-        );
+        )
     }
+}
+
+PostItem.defaultProps = {
+    showActions: true
 }
 
 PostItem.propTypes = {
